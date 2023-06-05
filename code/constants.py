6acfe028f61d3d24f0efdde8305641f83b3e8a16
LuizGBR <bandeiralg13@gmail.com>
2023-06-05 11:14:54 -0300

    updating path to dataset

--- code/constants.py
+++ code/constants.py
@@ -9,4 +9,4 @@ Please, use this file to set the constants that will be used to load important l
 # The path to Raug. You may find it here: https://github.com/paaatcha/raug
 RAUG_PATH = "/home/lg/code/raug/"
 
-REDDIT_BASE_PATH = "/datasets"
+REDDIT_BASE_PATH = "/datasets/reddit"
